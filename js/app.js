9c0be10b129f2faca9678d9b0c95c1dbc5320f6f
Varin <94356640+VarinCode@users.noreply.github.com>
2023-05-25 20:36:22 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 let numID = 0;
 
 const resetNumID = () => {
-        document.querySelectorAll(`li`).forEach((el, idx) => {
-            el.id = `li-${idx}`;
-            el.children[1].children[0].id = idx;
-            el.children[2].children[0].id = idx;
-            el.children[2].children[1].id = idx;
-            el.children[2].children[2].id = idx;
-        })        
+    document.querySelectorAll(`li`).forEach((el, idx) => {
+        el.id = `li-${idx}`;
+        el.children[1].children[0].id = idx;
+        el.children[2].children[0].id = idx;
+        el.children[2].children[1].id = idx;
+        el.children[2].children[2].id = idx;
+    })
 }
 
 const deleteLI = (t) => {
@@ -28,9 +28,9 @@ const deleteLI = (t) => {
 
 const editLI = (t) => {
     resetNumID()
-    document.querySelectorAll('li').item(t.id).children[0].children[0].textContent.length === 1 ?
-    document.querySelectorAll('li').item(t.id).children[0].children[0].textContent = document.querySelector('#input').value :
-    document.querySelectorAll('li').item(t.id).children[0].children[0].textContent = t.value;
+    document.querySelectorAll('li').item(t.id).children[0].children[0].textContent.length === 1 || t.value === '' ?
+        document.querySelectorAll('li').item(t.id).children[0].children[0].textContent = document.querySelector('#input').value :
+        document.querySelectorAll('li').item(t.id).children[0].children[0].textContent = t.value;
 }
 
 const showInput = (t) => {
@@ -58,40 +58,57 @@ const checkbox = (t) => {
     t.checked ?
         addAndRemove(true, 'task', 'task-done', '.list-done') :
         addAndRemove(false, 'task-done', 'task', '.list');
-    if([...t.parentElement.parentElement.classList].includes("task-done")){
-        t.setAttribute('disabled' , 'disabled')
-        t.nextElementSibling.setAttribute('disabled' , 'disabled');        
+    if ([...t.parentElement.parentElement.classList].includes("task-done")) {
+        t.nextElementSibling.setAttribute('disabled', 'disabled');
         t.nextElementSibling.style.display = "none";
+    } else {
+        t.nextElementSibling.removeAttribute('disabled')
+        t.nextElementSibling.style.display = "inline";
     }
 }
 
 document.querySelector('form').addEventListener('submit', () => {
     event.preventDefault();
     let bool = true;
+    let maxList = 50;
     document.querySelectorAll('li').forEach((el) => {
         if (el.children[0].children[0].textContent.toString() === document.querySelector('#input').value) bool = !bool;
     })
     if (bool) {
-        const liEL = document.createElement('li');
-        const input = document.querySelector('#input').value;
-        document.querySelector('.list').appendChild(liEL);
-        liEL.setAttribute('id', `li-${numID}`)
-        liEL.classList.add("task");
-        liEL.innerHTML = `
-                <header>
-                    <h2 class="title">${input}</h2>      
-                    <p>${new Date().toDateString()}</p>      
-                </header>
-                <div class="input-group">
-                    <input type="text" class="" id="${numID}" value="${input}" oninput="editLI(this)" onmouseout="hiddenInput(this)" style="display:none" maxlength="50">            
-                </div>
-                <span class="btn-group">    
-                    <input type="checkbox" id="${numID}" onchange="checkbox(this)" class="check">   
-                    <button class="btn-edit" id="${numID}" onclick="showInput(this)"><i class="ri-file-edit-fill fa-g"></i></button>            
-                    <button class="btn-delete" id="${numID}" onclick="deleteLI(this)"><i class="ri-delete-bin-5-fill fa-g"></i></button>            
-                </span>
-        `
-        resetNumID(liEL.id);
+        console.log(document.querySelector('.list').childElementCount + 1);
+        if (document.querySelector('.list').childElementCount + 1 > maxList) {
+            Swal.fire({
+                icon: 'info',
+                title: 'information!',
+                html: 'No more to-do items can be added, only <p style="color:#37d4fc;">50 items</p> can be added.',
+            })
+        } else {
+            const liEL = document.createElement('li');
+            const input = document.querySelector('#input').value;
+            document.querySelector('.list').appendChild(liEL);
+            liEL.setAttribute('id', `li-${numID}`)
+            liEL.classList.add("task");
+            const date = {
+                dateStrig: new Date().toDateString(),
+                timeString: new Date().toLocaleTimeString()
+            }
+            Object.freeze(date);
+            liEL.innerHTML = `
+                    <header>
+                        <h2 class="title">${input}</h2>      
+                        <p>${date.dateStrig} ${date.timeString}</p>      
+                    </header>
+                    <div class="input-group">
+                        <input type="text" class="" id="${numID}" value="${input}" oninput="editLI(this)" onmouseout="hiddenInput(this)" style="display:none" maxlength="50">            
+                    </div>
+                    <span class="btn-group">    
+                        <input type="checkbox" id="${numID}" onchange="checkbox(this)" class="check">   
+                        <button class="btn-edit" id="${numID}" onclick="showInput(this)"><i class="ri-file-edit-fill fa-g"></i></button>            
+                        <button class="btn-delete" id="${numID}" onclick="deleteLI(this)"><i class="ri-delete-bin-5-fill fa-g"></i></button>            
+                    </span>
+            `
+            resetNumID();
+        }
     } else {
         Swal.fire({
             icon: 'error',
